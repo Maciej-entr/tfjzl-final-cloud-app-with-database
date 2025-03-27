654922dedf84e42327be3c4de706660ec8a014e9
Maciej Waszyński <m.waszynski2005@wp.pl>
2025-03-27 22:07:02 +0100

    final

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = [  # Add this for the empty path
+    path('', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
